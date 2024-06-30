8ec8a40a21e74992701b71bf4191df0b204302d6
Perez-JustineRyu-BSIT32E2 <160830731+Perez-JustineRyu-BSIT32E2@users.noreply.github.com>
2024-06-30 17:22:49 +0800

    eyyy
    happy birthday

---
+++ PREFINALSAUTH_PEREZ_JUSTINE_RYU/ProtectedApi/Controllers/ValuesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProtectedApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult Get()
+        {
+            
+            var userInfo = new
+            {
+                Name = "Justine Ryu S. Perez",
+                Section = "BSIT32E2",
+                Course = "Infortmation Technology"
+            };
+
+            var funFacts = new[]
+            {
+                "I Eat.",
+                "I Sleep.",
+                "I Pray.",
+            };
+
+            return Ok(new { UserInfo = userInfo, FunFacts = funFacts });
+        }
+    }
+}
